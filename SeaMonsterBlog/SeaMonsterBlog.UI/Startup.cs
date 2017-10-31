dfc417a9d82f6eb63afb0ecc4bfac740ae8c6f82
Jake Mercer <jake.mercer@gmail.com>
2017-10-31 10:28:37 -0400

    sea monster blog UI solution created

---
+++ SeaMonsterBlog/SeaMonsterBlog.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SeaMonsterBlog.UI.Startup))]
+namespace SeaMonsterBlog.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
