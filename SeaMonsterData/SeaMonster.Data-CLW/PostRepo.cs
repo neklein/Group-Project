03b7e3bc2e06ab07e610ae9deabb3dff23e00669
Nate Klein <natek21@gmail.com>
2017-10-31 10:11:21 -0400

    Added GetAllPosts and GetComments

--- SeaMonsterData/SeaMonster.Data-CLW/PostRepo.cs
+++ SeaMonsterData/SeaMonster.Data-CLW/PostRepo.cs
@@ -1,6 +1,8 @@
 ﻿using SeaMonster.Models.Tables;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -71,12 +73,85 @@ namespace SeaMonster.Data_CLW
 
         public List<Post> GetAllPost()
         {
-            throw new NotImplementedException();
+            List<Post> posts = new List<Post>();
+
+            string cs = "Server=localhost;Database=SeaMonster;User Id=SeamonsterSA; Password=ocean;";
+
+            using (var cn = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("GetPublishedPosts", cn);
+                SqlCommand cmdImage = new SqlCommand("GetImagesForPost", cn);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmdImage.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        Post post = new Post();
+
+                        post.PostId = (int)dr["PostID"];
+                        post.PostTitle = dr["PostTitle"].ToString();
+                        post.DateCreated = DateTime.Parse(dr["DateCreated"].ToString());
+                        post.ToPostDate = DateTime.Parse(dr["ToPostDate"].ToString());
+                        post.PostText = dr["PostText"].ToString();
+
+                        cmdImage.Parameters.AddWithValue("@PostID", post.PostId);
+
+                        using (SqlDataReader dr1 = cmdImage.ExecuteReader())
+                        {
+                            while (dr1.Read())
+                            {
+                                Image image = new Image();
+                                image.ImageId = (int)dr1["ImageID"];
+                                image.ImageName = dr["ImageName"].ToString();
+
+                                post.Images.Add(image);
+                            }
+                        }
+                        posts.Add(post);
+                    }
+                }
+            }
+            return posts;
+
         }
 
         public List<Comment> GetPublishedComments(int PostID)
         {
-            throw new NotImplementedException();
+            List<Comment> comments = new List<Comment>();
+
+
+            string cs = "Server=localhost;Database=SeaMonster;User Id=SeamonsterSA; Password=ocean;";
+
+            using (var cn = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("GetCommentbyID", cn);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@PostID", PostID);
+
+                cn.Open();
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        Comment comment = new Comment();
+                        comment.CommenterName = dr["CommenterName"].ToString();
+                        comment.CommentDate = DateTime.Parse(dr["CommentDate"].ToString());
+                        comment.CommentText = dr["CommentText"].ToString();
+
+                        comments.Add(comment);
+                    }
+                }
+
+            }
+
+
+            return comments;
+
         }
 
         public List<Reply> GetPublishedReplies(int CommentID)
